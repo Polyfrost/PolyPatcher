666dcdb4c20665f878c5928cb6634accc45dee3d
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-07-14 13:24:27 -0700

    add multiversioning

--- src/main/java/club/sk1er/patcher/mixins/performance/forge/OBJLoaderMixin_UnclosedStream.java
+++ src/main/java/club/sk1er/patcher/mixins/performance/forge/OBJLoaderMixin_UnclosedStream.java
@@ -3,28 +3,26 @@ package club.sk1er.patcher.mixins.performance.forge;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IModel;
-import net.minecraftforge.client.model.b3d.B3DLoader;
+import net.minecraftforge.client.model.obj.OBJLoader;
 import org.apache.commons.io.IOUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-import java.io.InputStream;
-
-@Mixin(value = B3DLoader.class, remap = false)
-public class B3DLoaderMixin_UnclosedStream {
+@Mixin(value = OBJLoader.class, remap = false)
+public class OBJLoaderMixin_UnclosedStream {
     //#if MC==10809
     @Unique
     private IResource patcher$leakingResource;
 
     // for some reason localcapture doesn't work here, so let's just store it locally like this
-    @Redirect(method = "loadModel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/IResource;getInputStream()Ljava/io/InputStream;"))
-    private InputStream patcher$getStream(IResource instance) {
-        this.patcher$leakingResource = instance;
-        return instance.getInputStream();
+    @ModifyArgs(method = "loadModel", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/model/obj/OBJModel$Parser;<init>(Lnet/minecraft/client/resources/IResource;Lnet/minecraft/client/resources/IResourceManager;)V"))
+    private void patcher$getStream(Args args) {
+        this.patcher$leakingResource = args.get(0);
     }
 
     @Inject(method = "loadModel", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
