5aea8a8b80195109b5922f4d5b2ac920596007d6
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-07-14 02:26:46 -0700

    fix multiversion

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/WorldRendererMixin_GrowBuffer.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/WorldRendererMixin_GrowBuffer.java
@@ -1,6 +1,5 @@
 package club.sk1er.patcher.mixins.bugfixes;
 
-//#if MC==10809
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,16 +7,21 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(WorldRenderer.class)
-//#endif
+@Mixin(
+    //#if MC==10809
+    WorldRenderer.class
+    //#else
+    //$$ net.minecraft.client.Minecraft.class
+    //#endif
+)
 public abstract class WorldRendererMixin_GrowBuffer {
+    //#if MC==10809
     @Shadow
     protected abstract void growBuffer(int p_181670_1_);
 
     @Shadow
     private VertexFormat vertexFormat;
 
-    //#if MC==10809
     @Redirect(method = "addVertexData", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/WorldRenderer;growBuffer(I)V"))
     private void patcher$expandBuffer(WorldRenderer instance, int j, int[] vertexData) {
         this.growBuffer(vertexData.length + this.vertexFormat.getNextOffset());
