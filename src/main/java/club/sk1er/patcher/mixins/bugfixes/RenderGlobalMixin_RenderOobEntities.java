73d28766f3ce4f1ea4ba8545cecd5771123f8e7e
ev chang <wyvestbusiness@gmail.com>
2024-07-16 23:14:22 +0900

    Remove unnecessary BlockPos creation

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/RenderGlobalMixin_RenderOobEntities.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/RenderGlobalMixin_RenderOobEntities.java
@@ -5,7 +5,7 @@ import net.minecraft.client.renderer.chunk.RenderChunk;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.BlockPos;
+import net.minecraft.util.MathHelper;
 import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,13 +15,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class RenderGlobalMixin_RenderOobEntities {
     //#if MC==10809
     // fixes entities outside the world (such as below y0 or above y255) not rendering at specific camera angles
-    @Redirect(method = "setupTerrain", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/chunk/RenderChunk;boundingBox:Lnet/minecraft/util/AxisAlignedBB;", opcode = Opcodes.GETFIELD))
+    @Redirect(method = "setupTerrain", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/chunk/RenderChunk;boundingBox:Lnet/minecraft/util/AxisAlignedBB;", opcode = Opcodes.GETFIELD, ordinal = 0))
     private AxisAlignedBB patcher$fixOobEntityRendering(RenderChunk instance, Entity viewEntity, double partialTicks, ICamera camera, int frameCount, boolean playerSpectator) {
-        double d3 = viewEntity.lastTickPosX + (viewEntity.posX - viewEntity.lastTickPosX) * partialTicks;
         double d4 = viewEntity.lastTickPosY + (viewEntity.posY - viewEntity.lastTickPosY) * partialTicks;
-        double d5 = viewEntity.lastTickPosZ + (viewEntity.posZ - viewEntity.lastTickPosZ) * partialTicks;
-        BlockPos blockpos1 = new BlockPos(d3, d4 + (double)viewEntity.getEyeHeight(), d5);
-        return instance.boundingBox.addCoord(0.0, blockpos1.getY() > 0 ? Double.POSITIVE_INFINITY : Double.NEGATIVE_INFINITY, 0.0);
+        return instance.boundingBox.addCoord(0.0, MathHelper.floor_double(d4 + (double)viewEntity.getEyeHeight()) > 0 ? Double.POSITIVE_INFINITY : Double.NEGATIVE_INFINITY, 0.0);
     }
     //#endif
 }
