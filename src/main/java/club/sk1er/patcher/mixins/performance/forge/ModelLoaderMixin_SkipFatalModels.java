da5370a9b4fe27cd88309dfc2e714e7d5b672241
ev chang <wyvestbusiness@gmail.com>
2024-07-17 19:03:54 +0900

    more preprocessor fixes

--- src/main/java/club/sk1er/patcher/mixins/performance/forge/ModelLoaderMixin_SkipFatalModels.java
+++ src/main/java/club/sk1er/patcher/mixins/performance/forge/ModelLoaderMixin_SkipFatalModels.java
@@ -5,9 +5,11 @@ import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.client.resources.model.IBakedModel;
+//#if MC==10809
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.util.IRegistry;
+//#endif
 import net.minecraftforge.client.model.ModelLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,10 +17,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ModelLoader.class)
-public abstract class ModelLoaderMixin_SkipFatalModels extends ModelBakery implements FMLClientHandlerExt {
+public abstract class ModelLoaderMixin_SkipFatalModels
+    //#if MC==10809
+    extends ModelBakery implements FMLClientHandlerExt
+    //#endif
+{
+    //#if MC==10809
     public ModelLoaderMixin_SkipFatalModels(IResourceManager p_i46085_1_, TextureMap p_i46085_2_, BlockModelShapes p_i46085_3_) {
         super(p_i46085_1_, p_i46085_2_, p_i46085_3_);
     }
+    //#endif
 
     //#if MC==10809
     @Inject(method = "setupModelRegistry", at = @At("HEAD"), cancellable = true)
