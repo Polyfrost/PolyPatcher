40daa0bc93d978d0da185b1ea73a631d4e49de71
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-07-21 14:36:22 -0700

    fix MC-53850

---
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/EntityExpOrbMixin_RenderDamagedInvulnerables.java
@@ -0,0 +1,28 @@
+package club.sk1er.patcher.mixins.bugfixes;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityXPOrb.class)
+public abstract class EntityExpOrbMixin_RenderDamagedInvulnerables
+    //#if MC==10809
+    extends Entity
+    //#endif
+{
+    //#if MC==10809
+    public EntityExpOrbMixin_RenderDamagedInvulnerables(World worldIn) {
+        super(worldIn);
+    }
+
+    @Inject(method = "attackEntityFrom", at = @At("HEAD"), cancellable = true)
+    private void patcher$properInvulnerableCheck(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (this.worldObj.isRemote || this.isDead) cir.setReturnValue(false);
+    }
+    //#endif
+}
---
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/EntityItemMixin_RenderDamagedInvulnerables.java
@@ -0,0 +1,28 @@
+package club.sk1er.patcher.mixins.bugfixes;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityItem.class)
+public abstract class EntityItemMixin_RenderDamagedInvulnerables
+    //#if MC==10809
+    extends Entity
+    //#endif
+{
+    //#if MC==10809
+    public EntityItemMixin_RenderDamagedInvulnerables(World worldIn) {
+        super(worldIn);
+    }
+
+    @Inject(method = "attackEntityFrom", at = @At("HEAD"), cancellable = true)
+    private void patcher$properInvulnerableCheck(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (this.worldObj.isRemote || this.isDead) cir.setReturnValue(false);
+    }
+    //#endif
+}
